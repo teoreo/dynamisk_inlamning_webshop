0e1e36a457476c5b279be5758c30ba7416c2a9f5
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-03-02 16:45:50 +0100

    adding and editing the ejs-files

--- src/server.js
+++ src/server.js
@@ -6,8 +6,13 @@ const port = process.env.PORT || 2000;
 
 const productItem = require("../model/product");
 
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// get customer login
+app.get("/login", async (req, res) => {
+    res.render("login");
+})
 
 module.exports = { app, port, express };
