8a1cd695e6382d112e4ff215b916eda4cd07aad6
Emma Teo <emmateo@Emmas-MBP-3.mediegruppen.se>
2020-03-04 11:39:53 +0100

    nya branchen

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+const config = require("./config/config")
 const { app, port } = require("./src/server");
 
 const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true };
-mongoose.connect(dbUrl, dbOptions).then(() => {
+mongoose.connect(config.databaseURL, dbOptions).then(() => {
   app.listen(port, () => console.log(`App listening on port ${port}!`));
 });
 
--- src/server.js
+++ src/server.js
@@ -1,13 +1,44 @@
 const express = require("express");
-
+const path = require("path")
 const app = express();
 
 const port = process.env.PORT || 2000;
 
 const productItem = require("../model/product");
 
-app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+// ADMIN
+
+app.get("/admin/createproducts", (req,res)=> {
+    res.render("admin/createproducts")
+})
+app.post("/admin/createproducts", async (req,res) =>{
+    const addProduct = new productItem({
+        course: req.body.course,
+        title: req.body.course,
+        image: req.body.image,
+        price: req.body.price,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        weeks: req.body.weeks
+    })
+    await addProduct.save((error,success)=>{
+    if (error){
+      error? res.send(error.message): res.redirect("/todo")
+    }
+    else
+    res.redirect("/admin/createproducts")
+})
+  });
+
+
+
+
+
+
 
 module.exports = { app, port, express };
