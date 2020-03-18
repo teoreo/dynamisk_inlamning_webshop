2d9b9c6b16f51869c66c65325adb521336e23ae9
[stefanhallberg123]”git config --global user.name [stefanhallberg123]” <[stefanhalllberg@live.se]>
2020-03-18 11:36:01 +0100

    working on admin before pull

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -19,7 +19,7 @@ const adminROUTE = {
     // signup: "/admin/signup",
     welcome: "/admin/welcome",
     products: "/admin/products",
-    createproduct: "/admin/createproducts",
+    addproduct: "/admin/addproduct",
     editproduct: "/admin/editproduct",
     deleteproduct: "/admin/deleteproduct/:id",
     orders: "/admin/orders",
@@ -33,7 +33,7 @@ const adminVIEW = {
     // signup: "admin/signup",
     welcome: "admin/welcome",
     products: "admin/products",
-    createproduct: "admin/createproduct",
+    addproduct: "admin/addproduct",
     editproduct: "admin/editproduct",
     orders: "admin/orders",
     editorders: "admin/editorders/:id",
@@ -90,14 +90,14 @@ router.get(adminROUTE.products, (req, res) => {
 });
 
 router.post(adminROUTE.products, async (req, res) => {
-    
+
 });
 
-// admin createproduct
-router.get("/admin/createproducts", (req, res) => {
-    res.render("admin/createproducts")
+// admin addproduct
+router.get(adminROUTE.addproduct, (req, res) => {
+    res.render(adminVIEW.addproduct)
 })
-router.post("/admin/createproducts", async (req, res) => {
+router.post(adminROUTE.addproduct, async (req, res) => {
     const addProduct = new productItem({
         course: req.body.course,
         title: req.body.title,
@@ -109,23 +109,21 @@ router.post("/admin/createproducts", async (req, res) => {
     })
     await addProduct.save((error, success) => {
         if (error) {
-            error ? res.send(error.message) : res.redirect("/todo")
+            error ? res.send(error.message) : res.redirect(adminROUTE.addproduct)
         }
-        else
-            res.redirect("/admin/createproducts")
     })
 });
 // admin editproduct
 router.get(adminROUTE.editproduct, async (req, res) => {
-    const response = await productItem.findById({_id:req.params.id})
-    res.render(adminVIEW.editproduct, {response})
+    const response = await productItem.findById({ _id: req.params.id })
+    res.render(adminVIEW.editproduct, { response })
 })
 
-router.post(adminROUTE.editproduct, async (req,res)=>{
-    await productItem.updateOne({_id:req.body._id},
-        {$set: {text: req.body.text, done:req.body.done}},
-    {runValidators:true}, (error)=> error? res.send(error.message): res.redirect(adminROUTE.products) 
-    
+router.post(adminROUTE.editproduct, async (req, res) => {
+    await productItem.updateOne({ _id: req.body._id },
+        { $set: { text: req.body.text, done: req.body.done } },
+        { runValidators: true }, (error) => error ? res.send(error.message) : res.redirect(adminROUTE.products)
+
     )
 })
 
