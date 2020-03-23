07c1c6d91912d62ce66d2f799fd556aaa1b864a5
[isabelleedlund] <[edlund.isabelle@gmail.com]>
2020-03-23 09:16:23 +0100

    Added so added course is shown to customer, Isabelle, Emma, Stefan, Julia & Josefin

--- router/adminRouter.js
+++ router/adminRouter.js
@@ -7,8 +7,12 @@ const productItem = require("../model/product");
 // middleware
 const router = express();
 
-router.use(bodyParser.urlencoded({ extended: false }))
-router.use(express.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({
+    extended: false
+}))
+router.use(express.urlencoded({
+    extended: true
+}));
 router.set("view engine", "ejs");
 router.use(express.static("public"));
 
@@ -64,7 +68,9 @@ router.get(adminROUTE.login, async (req, res) => {
     //     password: "admin"
     // }).save();
     const errorMessage = ""
-    res.render(adminVIEW.login, { errorMessage });
+    res.render(adminVIEW.login, {
+        errorMessage
+    });
 });
 
 router.post(adminROUTE.login, async (req, res) => {
@@ -72,7 +78,9 @@ router.post(adminROUTE.login, async (req, res) => {
         email: req.body.loginemail,
         password: req.body.loginpassword
     })
-    if (!admin) return res.render(adminVIEW.login, { errorMessage: "You are not an admin or wrong password" })
+    if (!admin) return res.render(adminVIEW.login, {
+        errorMessage: "You are not an admin or wrong password"
+    })
     res.redirect(adminROUTE.welcome);
 });
 
@@ -93,25 +101,38 @@ router.get(adminROUTE.products, async (req, res) => {
 
     const allProducts = await productItem.find();
 
-    const fiveProducts = await productItem.find().sort({ date: sortByDate }).skip((currentPage - 1) * productPerPage).limit(productPerPage)
+    const fiveProducts = await productItem.find().sort({
+        date: sortByDate
+    }).skip((currentPage - 1) * productPerPage).limit(productPerPage)
     const pagesCount = Math.ceil(allProducts.length / productPerPage)
 
-    res.render(adminVIEW.products, { fiveProducts, pagesCount, currentPage });
+    res.render(adminVIEW.products, {
+        fiveProducts,
+        pagesCount,
+        currentPage
+    });
 });
 
 router.get(adminROUTE.deleteproduct, async (req, res) => {
-    await productItem.deleteOne({ _id: req.params.id });
+    await productItem.deleteOne({
+        _id: req.params.id
+    });
     res.redirect(adminROUTE.products)
 });
 
 router.get(adminROUTE.editproduct, async (req, res) => {
-    const response = await productItem.findById({ _id: req.params.id });
-    res.render(adminVIEW.editproduct, { response })
+    const response = await productItem.findById({
+        _id: req.params.id
+    });
+    res.render(adminVIEW.editproduct, {
+        response
+    })
 });
 
 router.post(adminROUTE.editproduct, async (req, res) => {
-    await productItem.updateOne({ _id: req.body._id },
-        {
+    await productItem.updateOne({
+            _id: req.body._id
+        }, {
             $set: {
                 title: req.body.title,
                 price: req.body.price,
@@ -144,10 +165,13 @@ router.post(adminROUTE.addproduct, async (req, res) => {
         price: req.body.price,
         description: req.body.description,
         quantity: req.body.quantity,
-        weeks: req.body.weeks,
+        weekone: req.body.weekone,
+        weektwo: req.body.weektwo,
+        weekthree: req.body.weekthree,
+        weekfour: req.body.weekfour,
         country: req.body.country
-
-    })
+    });
+    
     await addProduct.save((error, success) => {
         if (error) {
             res.send(error.message)
@@ -159,14 +183,25 @@ router.post(adminROUTE.addproduct, async (req, res) => {
 });
 // admin editproduct
 router.get(adminROUTE.editproduct, async (req, res) => {
-    const response = await productItem.findById({ _id: req.params.id })
-    res.render(adminVIEW.editproduct, { response })
+    const response = await productItem.findById({
+        _id: req.params.id
+    })
+    res.render(adminVIEW.editproduct, {
+        response
+    })
 })
 
 router.post(adminROUTE.editproduct, async (req, res) => {
-    await productItem.updateOne({ _id: req.body._id },
-        { $set: { text: req.body.text, done: req.body.done } },
-        { runValidators: true }, (error) => error ? res.send(error.message) : res.redirect(adminROUTE.products)
+    await productItem.updateOne({
+            _id: req.body._id
+        }, {
+            $set: {
+                text: req.body.text,
+                done: req.body.done
+            }
+        }, {
+            runValidators: true
+        }, (error) => error ? res.send(error.message) : res.redirect(adminROUTE.products)
 
     )
 })
