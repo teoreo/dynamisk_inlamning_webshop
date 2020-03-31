73ce00bd6b84a56275aac827a05f84ae47f406d1
[teoreo] <[emma.teo@medieinstitutet.se]>
2020-03-31 13:48:01 +0200

    testing

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 if (process.env.NODE_ENV !== "production") require('dotenv').config()
 const config = {
     databaseURL:
-      "mongodb+srv://stefanhallberg:surf123@surf-pyr2p.mongodb.net/test?retryWrites=true&w=majority",
-      mail:"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"
+      process.env.DATABASE,
+      mail: process.env.MAIL
   };
   
   module.exports = config;
